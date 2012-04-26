4f4fad286c0c7126a7a8fe9f12fed6993e83588a
tjcjc <taijcjc@gmail.com>
2012-04-26 16:35:58 +0800

    update register

--- lib/redis_fragment_cache/register.rb
+++ lib/redis_fragment_cache/register.rb
@@ -1,3 +1,18 @@
-require 'rails'
 require './init_fragment_key_methods'
-Rails::Application.send(:include, RedisFragmentCache::InitFragmentKeyMethods)
+module ActionView::Helpers
+  include RedisFragmentCache::InitFragmentKeyMethods
+  init_fragment_key_methods
+  init_set_key_methods
+end
+
+class ActiveRecord::Base
+  include RedisFragmentCache::InitFragmentKeyMethods
+  init_fragment_key_methods
+  init_set_key_methods
+end
+
+class ActionController::Base
+  include RedisFragmentCache::InitFragmentKeyMethods
+  init_fragment_key_methods
+  init_set_key_methods
+end
